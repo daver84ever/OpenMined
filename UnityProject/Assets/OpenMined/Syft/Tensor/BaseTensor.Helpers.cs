5f4a51c155672aa737689f1f82fb594d0360a76f
daver84ever <dave@eppygames.com>
2018-02-01 22:30:58 -0800

    Implemented CPU Tensor Sort
    Tensor Sort alg. for the CPU. Uses optimized .NET implementation. Sort alg in BaseTensor passed in IComparer in childern class Tensors. Allows for Acending and Decending order and easily expandeable for more complex tensor types.

---
+++ UnityProject/Assets/OpenMined/Syft/Tensor/BaseTensor.Helpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenMined.Syft.Tensor
+{
+	public partial class BaseTensor 
+	{
+		public enum SortOrder { Ascending, Descending };
+
+		protected void Sort<T>(T[] elements, IComparer<T> comparer) 
+		{/*
+			IComparer comparer;
+			switch (order)
+			{
+
+				case: SortOrder.Descending;
+					
+					break;
+				case: SortOrder.Ascending;
+				default:
+
+					break;
+			}
+			*/
+			Array.Sort(elements, comparer);
+		}
+
+
+
+		private class SortAscendingHelper : IComparer<float>
+		{
+			public int Compare(float x, float y)
+			{
+				if (x > y)
+				{
+					return 1;
+				} 
+
+				if (y > x)
+				{
+					return -1;
+				}
+
+				return 0;
+			}
+		}
+}
+
